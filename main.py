42c6b9bc01c0bad7fdd7e217a947a546edf8e0ba
Andriy Gula <gula@pr.km.ua>
2023-02-27 20:12:06 +0200

    add backend and homework

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 st.title('Weather Forecast for the Next Days')
 place = st.text_input('Place: ')
@@ -11,15 +12,26 @@ option = st.selectbox('Select data to view',
                          ('Temperature', 'Sky'))
 st.subheader(f'{option} the next {days} days in {place}')
 
-def get_data(days):
-    dates = ['2022-25-10', '2022-26-10', '2022-27-10']
-    temperatures = [10, 11, 15]
-    temperatures = [days * i for i in temperatures]
-    return dates, temperatures
+if place:
+    try:
+        filtered_data = get_data(place, days)
 
-dates, temperatures = get_data(days)
+        if option == 'Temperature':
+            temperatures = [dict['main']['temp'] - 273.15 for dict in filtered_data]
+            dates = [dict['dt_txt'] for dict in filtered_data] 
+            figure = px.line(x=dates,
+                                y=temperatures,
+                                labels={'x': 'Date', 'y': 'Temperature (C)'})
+            st.plotly_chart(figure)
 
-figure = px.line(x=dates,
-                    y=temperatures,
-                    labels={'x': 'Date', 'y': 'Temperature (C)'})
-st.plotly_chart(figure)
+        if option == 'Sky':
+            sky_conditions = [dict['weather'][0]['main'] for dict in filtered_data]
+            images = {'Clear': 'images/clear.png',
+                        'Clouds': 'images/cloud.png',
+                        'Rain': 'images/rain.png',
+                        'Snow': 'images/snow.png'}
+            img_paths = [images[condition] for condition in sky_conditions]
+            st.image(img_paths, width=80)
+    
+    except KeyError:
+        st.warning('Wrong place')
